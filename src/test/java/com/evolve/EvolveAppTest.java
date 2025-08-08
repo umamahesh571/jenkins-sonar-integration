86ac65707c78c9763122e4e65ad2d44ecb177456
umamahesh571 <112250747+umamahesh571@users.noreply.github.com>
2025-08-08 16:54:12 +0530

    Update EvolveAppTest.java

--- src/test/java/com/evolve/EvolveAppTest.java
+++ src/test/java/com/evolve/EvolveAppTest.java
@@ -1,17 +1,16 @@
 package com.evolve;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests for EvolveApp.
  */
-public class EvolveAppTest {
+class EvolveAppTest {
 
     @Test
-    public void testGetWelcomeMessage() {
-        EvolveApp app = new EvolveApp();
-        String expected = "WELCOME TO EVOLVE TECHNOLOGIES\nHAPPY LEARNING........................";
-        assertEquals(expected, app.getWelcomeMessage());
+    void testWelcomeMessage() {
+        String expected = "WELCOME TO EVOLVE TECHNOLOGIES" + System.lineSeparator() + "HAPPY LEARNING........................";
+        assertEquals(expected, EvolveApp.getWelcomeMessage());
     }
 }
