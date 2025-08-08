b47683e2d7bccaf03714beb6f227daceabcc0d59
umamahesh571 <112250747+umamahesh571@users.noreply.github.com>
2025-08-08 16:03:28 +0530

    Add files via upload

---
+++ src/main/java/com/evolve/EvolveApp.java
@@ -0,0 +1,16 @@
+
+package com.evolve;
+
+public class EvolveApp {
+    public String welcomeMessage() {
+        return "WELCOME TO EVOLVE TECHNOLOGIES";
+    }
+
+    public String learningMessage() {
+        return "HAPPY LEARNING........................";
+    }
+
+    public boolean isActive() {
+        return true;
+    }
+}
