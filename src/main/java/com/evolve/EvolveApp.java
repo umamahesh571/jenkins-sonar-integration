eebc3094bf3a95ec488defd379041ae817892642
umamahesh571 <112250747+umamahesh571@users.noreply.github.com>
2025-08-08 16:37:19 +0530

    Update EvolveApp.java

--- src/main/java/com/evolve/EvolveApp.java
+++ src/main/java/com/evolve/EvolveApp.java
@@ -1,16 +1,18 @@
 package com.evolve;
 
 public class EvolveApp {
-    public String getWelcomeMessage() {
-        return "WELCOME TO EVOLVE TECHNOLOGIES\nHAPPY LEARNING........................";
+
+    public String welcomeMessage() {
+        return "WELCOME TO EVOLVE TECHNOLOGIES";
     }
 
-    public boolean isLearningFun() {
-        return true;
+    public String learningMessage() {
+        return "HAPPY LEARNING........................";
     }
 
     public static void main(String[] args) {
         EvolveApp app = new EvolveApp();
-        System.out.println(app.getWelcomeMessage());
+        System.out.println(app.welcomeMessage());
+        System.out.println(app.learningMessage());
     }
 }
