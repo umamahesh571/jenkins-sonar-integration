3969a186e4e14f6221287f47707db31124fc78b0
umamahesh571 <112250747+umamahesh571@users.noreply.github.com>
2025-08-08 15:51:48 +0530

    Delete src directory

--- src/main/java/com/evolve/EvolveApp.java
+++
@@ -1,8 +0,0 @@
-package com.evolve;
-
-public class EvolveApp {
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO EVOLVE TECHNOLOGIES");
-        System.out.println("HAPPY LEARNING........................");
-    }
-}
